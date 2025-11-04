083ab021d558fae46ab2f49ac13e59dcaa8fd19f
GilbertR-Programmer <173060871+GilbertR-Programmer@users.noreply.github.com>
2025-11-04 17:12:19 +0000

    Auto Increment Version

--- runScript.js
+++ runScript.js
@@ -217,7 +217,7 @@ function generateCharacter() {
 
 // Code For Adding The Footer
 const footer =
-  "We be wizards V39, Use however you want, Created by Gilbert Reid";
+  "We be wizards V40, Use however you want, Created by Gilbert Reid";
 
 // Function to generate a new character with a random name and random spells
 function addFooter() {
